2355aeda5f32a5f50d9f9729acadcd16ce788c91
Thomas Leung <85861640+Thomas-Leung-852@users.noreply.github.com>
2025-08-25 23:37:18 +0800

    Version 1.4.0
    Added
    - PKCS#12 (P12) keystore contain a self-signed certificate
    - Enable HTTPS connection

--- housekeeping_bee/files/prog/run3.sh
+++ housekeeping_bee/files/prog/run3.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+
+
 export housekeeper_bee_sys_img=~/Desktop/housekeeping_bee/files/sys/images/
 export housekeeper_bee_upload_path=~/Desktop/housekeeping_bee/files/uploads/
 export housekeeper_bee_backup_restore=~/Desktop/housekeeping_bee/backup/
@@ -10,5 +14,6 @@ export email_api_key_prod=xxx
 export sender_email_address=support@a.com
 export server_location=onsite
 export license_key=~/Desktop/housekeeping_bee/files/prog/license.yaml
+export keystore_pwd=abc123
 cd ~/Desktop/housekeeping_bee/files/prog
 java -jar housekeeper-0.0.1-SNAPSHOT.jar --spring.config.location=application.properties
